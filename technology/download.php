df9701c270e7413cec17bb0f61775eb82f1a3f13
ThapsonNyirenda <95896174+ThapsonNyirenda@users.noreply.github.com>
2022-01-21 08:03:55 -0800

    add files via upload

---
+++ technology/download.php
@@ -0,0 +1,43 @@
+<?php
+
+    $conn=mysqli_connect('localhost','root','','school');
+
+    if (isset($_GET['file'])) {
+        if(is_numeric($_GET['file']))
+        {
+            $id=$_GET['file'];
+
+            $sql="SELECT * FROM technology WHERE ID=$id LIMIT 1";
+            $result1=mysqli_query($conn,$sql);
+
+            $result=mysqli_fetch_array($result1);
+
+            if (empty($result)) {
+                exit('file not found');
+            }else {
+
+                $file ='uploads/'.$result['FILE_NAMES'];
+
+                if (file_exists($file)) {
+                    header('Content-Type: '.mime_content_type($file));
+                    header('Content-Disposition: attachment; filename='.basename($file));
+                    header('Content-Length: '.filesize($file));
+                    header('Cache-Control: no-cache');
+                    header('Content-Transfer-Encoding: binary');
+
+                    readfile($file);
+                    $sql1="UPDATE technology SET DOWNLOADS=DOWNLOADS+1 WHERE ID=$id";
+                $result2=mysqli_query($conn,$sql1);
+                    header('location: table.php');
+                    exit;
+                }else {
+                    die ('file is not available');
+                }
+
+                
+            }
+        }
+    }else {
+        echo 'file is not set';
+    }
+?>
